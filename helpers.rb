68a150028bf6fbda95a3a07a474e1643524a23b5
Maimer <nesnl23@yahoo.com>
2014-05-23 22:23:37 -0400

    Added leaderboard and team page

--- helpers.rb
+++ helpers.rb
@@ -1,28 +1,51 @@
 def make_data()
-  [
-    {
-      home_team: "Patriots",
-      away_team: "Broncos",
-      home_score: 7,
-      away_score: 3
-    },
-    {
-      home_team: "Broncos",
-      away_team: "Colts",
-      home_score: 3,
-      away_score: 0
-    },
-    {
-      home_team: "Patriots",
-      away_team: "Colts",
-      home_score: 11,
-      away_score: 7
-    },
-    {
-      home_team: "Steelers",
-      away_team: "Patriots",
-      home_score: 7,
-      away_score: 21
-    }
-  ]
+  dataset = [
+              {
+                home_team: "Patriots",
+                away_team: "Broncos",
+                home_score: 7,
+                away_score: 3
+              },
+              {
+                home_team: "Broncos",
+                away_team: "Colts",
+                home_score: 3,
+                away_score: 0
+              },
+              {
+                home_team: "Patriots",
+                away_team: "Colts",
+                home_score: 11,
+                away_score: 7
+              },
+              {
+                home_team: "Steelers",
+                away_team: "Patriots",
+                home_score: 7,
+                away_score: 21
+              }
+            ]
 end
+
+def team_data(data)
+  records = {}
+
+  data.each do |game|
+    if !records.has_key?(game[:home_team])
+      records[game[:home_team]] = [0, 0]
+    end
+    if !records.has_key?(game[:away_team])
+      records[game[:away_team]] = [0, 0]
+    end
+    if game[:home_score] > game[:away_score]
+      records[game[:home_team]][0] += 1
+      records[game[:away_team]][1] += 1
+    else
+      records[game[:home_team]][1] += 1
+      records[game[:away_team]][0] += 1
+    end
+  end
+  records
+end
+
+puts team_data(make_data).inspect
--- server.rb
+++ server.rb
@@ -8,10 +8,15 @@ end
 get '/leaderboard' do
   @data = make_data()
   @title = "Game Time Leaderboard"
+  @teamdata = team_data(@data).sort_by { |team, record| [-record[0], record[1]] }
+
   erb :'leaderboard/leaderboard'
 end
 
-get 'teams/:team' do
+get '/teams/:team' do
   @title = params[:team]
   @data = make_data()
+  @teamdata = team_data(@data)
+
+  erb :'teams/team'
 end
